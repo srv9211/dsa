3a3b0658a5978bdabb79b7887572acd881c56c12
Sourav Sharma <sourav0852@gmail.com>
2025-01-07 11:42:32 +0530

    print-nums

---
+++ recursion/PrintNums.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PrintNums {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your input: ");
+        int n = scanner.nextInt();
+        System.out.println();
+
+        printNumbersAsc(n);
+//        printNumbersDes(n);
+    }
+
+    static void printNumbersAsc(int n) {
+        if (n == 0) return;
+
+        printNumbersAsc(n-1);
+        System.out.println(n);
+    }
+
+    static void printNumbersDes(int n) {
+        if (n == 0) return;
+
+        System.out.println(n);
+        printNumbersDes(n-1);
+    }
+}
